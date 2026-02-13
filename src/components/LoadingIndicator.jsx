016caece1a44276a4ea608f41d6afb07144ce2d6
Gopinath <kgopinath730@gmail.com>
2026-02-13 12:15:31 +0530

    Add files via upload
    uploaded frontend files

---
+++ src/components/LoadingIndicator.jsx
@@ -0,0 +1,11 @@
+function LoadingIndicator() {
+  return (
+    <div className="loading-indicator">
+      <span className="loading-dot"></span>
+      <span className="loading-dot"></span>
+      <span className="loading-dot"></span>
+    </div>
+  );
+}
+
+export default LoadingIndicator;
